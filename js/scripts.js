715c631a28797848a1151a37bf5e647016eeea91
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2022-10-20 16:12:10 +0300

    add and call subtract, multiply and divide functions

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,21 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
 //user interface logic
 const number1 = parseInt(prompt("Enter a number","00"));
 const number2 = parseInt(prompt("Enter another number","00"));
-window.alert(add(number1, number2));
+window.alert("The sum is " + add(number1, number2) + ".");
+window.alert("The difference is " + subtract(number1, number2) + ".");
+window.alert("The product is " + multiply(number1, number2) + ".");
+window.alert("The quotient is " + divide(number1, number2) + ".");
